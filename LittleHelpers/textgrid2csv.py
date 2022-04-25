9d7575d34662b22c5768aec87997141da58a152c
Dafydd Gibbon <dafyddg@users.noreply.github.com>
2022-04-25 19:25:43 +0200

    Update textgrid2csv.py
    Minor cosmetic changes.

--- LittleHelpers/textgrid2csv.py
+++ LittleHelpers/textgrid2csv.py
@@ -3,7 +3,7 @@
 # textgrid2csv.py, D. Gibbon, 2015.02.12
 # Convert Praat TextGrid to CSV
 import os, re, sys
-TAB = '\t'; NL = '\n'
+TAB = '\t'; NL = '\n'; SPACE = " "
 
 # Check input from CLI
 if len(sys.argv) < 2:
@@ -15,18 +15,18 @@ if not os.path.isfile(textgridfile):
     exit()
 csvfile = re.sub('.TextGrid','.csv',textgridfile)
 
-# Preprocess textgrid format
+# Read textgrid, preprocess textgrid format
 textgrid = open(textgridfile, 'r' ).read().split(NL)
 nugrid = []
 for line in textgrid:
-    line = re.sub(' *$','',line)    # Delete final spaces
-    line = re.sub('^ *','',line)    # Delete initial spaces
-    line = re.sub('\"','',line)     # Delete quotes
+    line = re.sub(' *$','',line) # Del. fin. spaces
+    line = re.sub('^ *','',line) # Del. init. spaces
+    line = re.sub('\"','',line)  # Delete quotes
     if line != '':
         nugrid += [line]
 
-# Initialise CSV text string
-csvstring = textgridfile + '\n'
+# Initialise CSV text string (metadata, column heads)
+csvstring = textgridfile + NL
 csvstring += TAB.join(
     ['Tier', 'Label', 'xmin', 'xmax', 'xdiff'] ) + NL
 
@@ -34,17 +34,20 @@ csvstring += TAB.join(
 rowflag = False
 
 # Loop through lines in preprocessed textgrid
+# The Praat indented hierarchy has finite depth
+# and can therefore be processed by an iterative
+# finite machine - recursion unnecessary:
 for line in nugrid:
-    if 'name = ' in line:               # Get tier name
-        name = line.split(" ")[-1]
-    if 'intervals [' in line:             # Skip the header
+    if 'name = ' in line:            # Get tier name
+        name = line.split(SPACE)[-1]
+    if 'intervals [' in line:        # Skip the header
     	rowflag = True
     if rowflag and 'xmin' in line:
-        xmin = '%.3f'%float(line.split(' ')[-1])
+        xmin = '%.3f'%float(line.split(SPACE)[-1])
     if rowflag and 'xmax' in line:
-        xmax = '%.3f'%float(line.split(' ')[-1])
+        xmax = '%.3f'%float(line.split(SPACE)[-1])
     if rowflag and "text" in line:
-        text = line.split(" ")[-1]
+        text = line.split(SPACE)[-1]
         xdiff = '%.3f'%(float(xmax)-float(xmin))
         row = TAB.join([name, text, xmin, xmax, xdiff])
         csvstring += row + NL
@@ -52,4 +55,3 @@ for line in nugrid:
         
 # Output CSV file
 open(csvfile,'w').write(csvstring)
-
