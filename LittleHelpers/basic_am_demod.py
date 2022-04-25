fba46432db023817d79557a66c775127bb7f6bc5
Dafydd Gibbon <dafyddg@users.noreply.github.com>
2022-04-25 18:21:08 +0200

    Update basic_am_demod.py
    Add minimal doc lines.

--- LittleHelpers/basic_am_demod.py
+++ LittleHelpers/basic_am_demod.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 import scipy.io.wavfile as wave
 from scipy.signal import medfilt
 
+# Input WAV file
 wavfilename = sys.argv[1]
 pngfilestem = re.sub(".wav","",wavfilename)
 appfilestem = re.sub(".py","",sys.argv[0])
@@ -16,27 +17,30 @@ period = 1/samplerate
 siglen = len(signal)
 sigsecs = siglen / samplerate
 
+# Extract AM envelope, apply FFT
 env = np.abs(signal)
 mags = np.abs(np.fft.rfft(env))
 freqs = np.fft.rfftfreq(env.size, period)
 
+# Select LF spectrum
 signal = signal / max(signal)
 env = medfilt(env / max(env), 301)
 mags = mags / max(mags)
+
+# Generate graphics
+maxfreq = 6
+maxsample = int(round(maxfreq*sigsecs))
+mags = mags[int(round(sigsecs/2)):maxsample]
+freqs = freqs[int(round(sigsecs/2)):maxsample]
 
 x = np.linspace(0, sigsecs, siglen)
 plt.subplot(3,1,1)	# waveform
 plt.plot(x, signal)
 plt.subplot(3,1,2)	# env
 plt.plot(x, env)
-
-maxfreq = 6
-maxsample = int(round(maxfreq*sigsecs))
-
 plt.subplot(3,1,3)	# env
-mags = mags[int(round(sigsecs/2)):maxsample]
-freqs = freqs[int(round(sigsecs/2)):maxsample]
 plt.plot(freqs, mags)
 
+# Save and show graphics
 plt.savefig("%s_%s.png"%(appfilestem, pngfilestem))
 plt.show()
